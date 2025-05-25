d850f80b6998fa1080717b910ed4d63cf4f87172
Shrikrishna Jagtap <shri9763@gmail.com>
2025-05-25 15:58:40 +0530

    added fib functionality

--- base.py
+++ base.py
@@ -5,3 +5,12 @@ def calculate_fact(num):
         return num * calculate_fact(num -1)
     
 print(calculate_fact(5))
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+# Example: print first 10 Fibonacci numbers
+fibonacci(10)
